54ba136e0cf092b89d03ad989c0102b2696c6996
ryan-aristosa <ryan.aristosa@softvision.com>
2023-02-27 17:42:20 +0800

    Add project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const bubbleHover = document.getElementById("bubble-hover");
+    const phrase = bubbleHover.innerText;
+
+    bubbleHover.innerText = "";
+
+    phrase.split("").map((char, index) => {
+      const span = document.createElement("span");
+
+      span.innerText = char;
+      span.setAttribute("data-index", index.toString());
+      span.classList.add("hover-char");
+
+      bubbleHover.appendChild(span);
+      return null;
+    });
+
+    const hoverChars = [...document.getElementsByClassName("hover-char")];
+
+    const removeClasses = () => {
+      hoverChars.map((char) => {
+        char.classList.remove("hovered");
+        char.classList.remove("hovered-adjacent");
+        return null;
+      });
+    };
+
+    hoverChars.map((char) => {
+      char.addEventListener("mouseover", (element) => {
+        removeClasses();
+
+        const currentElement = element.target;
+        const index = parseInt(currentElement.getAttribute("data-index"), 10);
+        const prevIndex = index === 0 ? null : index - 1;
+        const nextIndex = index === phrase.length - 1 ? null : index + 1;
+        const prevElement = prevIndex !== null && document.querySelector(`[data-index="${prevIndex}"]`);
+        const nextElement = nextIndex !== null && document.querySelector(`[data-index="${nextIndex}"]`);
+
+        currentElement.classList.add("hovered");
+        prevElement && prevElement.classList.add("hovered-adjacent");
+        nextElement && nextElement.classList.add("hovered-adjacent");
+      });
+
+      return null;
+    });
+
+    document.getElementById("bubble-hover").addEventListener("mouseleave", () => {
+      removeClasses();
+    });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="bubble-hover">Ryan Aristosa</h1>
     </div>
   );
 }
